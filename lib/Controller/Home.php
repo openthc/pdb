e0863055146a2be52be8d8f8eb011604e0f8f5d2
openthc <code@openthc.com>
2021-04-28 22:27:14 +0000

    Home page

---
+++ lib/Controller/Home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ */
+
+namespace App\Controller;
+
+class Home extends \OpenTHC\Controller\Base
+{
+	function __invoke($REQ, $RES, $ARG)
+	{
+		$data = [];
+		$file = 'home.php';
+		return $RES->write( $this->render($file, $data) );
+	}
+}
---
+++ view/home.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ */
+
+$_ENV['title'] = 'Cannabis Product Database :: OpenTHC';
+
+?>
+
+<div class="hero-wrap">
+	<div class="hero-core">
+		<h1>Cannabis Product Database</h1>
+		<h2>1,000+ verified product details</h2>
+		<h3>12000+ package and variety options &amp; suggestions</h3>
+	</div>
+</div>
+
+<main class="page-thin">
+
+	<section>
+		<h2>The Problem</h2>
+		<div class="card-body">
+
+			<p>
+			There are over 200 software providers across the world dealing with cannabis data.
+			Product and Package data-details, across the industry, would be nice to know, or at least keep consistent.
+			</p>
+
+			<p>
+			There are other offerings for a Product data API however, each has a unique, un-predictable numbering scheme.
+			Others were in existance but have been removed, or disabled.
+			</p>
+
+		</div>
+	</section>
+
+	<hr>
+
+	<section>
+		<h2 class="card-header">A Solution Proposal</h2>
+		<div class="card-body">
+			<p>A simple search API and static data-sets in multiple formats for any/all software providers to use.</p>
+			<p>A clearly defined normaliztion pattern.</p>
+			<p>A community driven submission and verification system.</p>
+			<p>An open license on the data to keep it available for everyone.</p>
+		</div>
+	</section>
+
+	<hr>
+
+	<div class="row">
+		<div class="col" style="text-align:center;">
+			<a class="btn btn-outline-success" href="/search">Browse Products</a>
+		</div>
+		<div class="col" style="text-align:center;">
+			<a class="btn btn-outline-success" href="/api">View API</a>
+		</div>
+	</div>
+
+	<hr>
+
+	<div class="download-link-list">
+		<div>
+			<a href="/pub/openthc-product-data.json"><i class="fa fa-download"></i> JSON</a>
+		</div>
+		<div>
+			<a href="/pub/openthc-product-data.sql"><i class="fa fa-download"></i> SQL</a>
+		</div>
+		<div>
+			<a href="/pub/openthc-product-data.csv"><i class="fa fa-download"></i> CSV</a>
+		</div>
+		<div>
+			<a href="/pub/openthc-product-data.tsv"><i class="fa fa-download"></i> TSV</a>
+		</div>
+		<div>
+			<a href="/pub/openthc-product-data.xml"><i class="fa fa-download"></i> XML</a>
+		</div>
+	</div>
+
+</main>
