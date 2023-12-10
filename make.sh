baa62f54a37c0c39f02288b068cfbb9c2575d7a9
openthc <code@openthc.com>
2023-12-10 19:17:36 +0000

    update home on make

--- make.sh
+++ make.sh
@@ -23,19 +23,17 @@ copy_bootstrap
 copy_fontawesome
 copy_jquery
 
-# mkdir -p webroot/vendor/jquery-ui/
-# cp node_modules/jquery-ui/dist/themes/base/jquery-ui.min.css webroot/vendor/jquery-ui/jquery-ui.min.css
-# cp node_modules/jquery-ui/dist/jquery-ui.js webroot/vendor/jquery-ui/jquery-ui.js
-
+#
+# Tailwind
 npx tailwindcss \
 	--input sass/base.css \
 	--output webroot/css/main.css
 
-o=${OPENTHC_ORIGIN:-}
-if [ -z "$o" ]
+OPENTHC_ORIGIN=$(php -r '$x = require_once("etc/config.php"); echo $x["openthc"]["pdb"]["origin"];')
+if [ -z "$OPENTHC_ORIGIN" ]
 then
 	echo "NO ORIGIN"
 	# exit 1
 else
-	curl "$o/home" > webroot/index.html
+	curl "$OPENTHC_ORIGIN/home" > webroot/index.html
 fi
