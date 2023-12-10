137fa13aa3ec3f23bcbfd3f24dc1ce46e07fd70c
openthc <code@openthc.com>
2023-12-10 18:46:54 +0000

    update dbc

--- boot.php
+++ boot.php
@@ -1,12 +1,52 @@
 <?php
 /**
  * Bootstrap Product Database
+ *
+ * This file is part of OpenTHC Product Database ("PDB").
+ *
+ * SDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * SDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SDB.  If not, see <https://www.gnu.org/licenses/>.*
  */
 
 define('APP_ROOT', __DIR__);
 
+error_reporting(E_ALL & ~E_NOTICE);
+
+openlog('openthc-pdb', LOG_ODELAY|LOG_PID, LOG_LOCAL0);
+
 require_once(APP_ROOT . '/vendor/autoload.php');
 
-\OpenTHC\Config::init(APP_ROOT);
+if ( ! \OpenTHC\Config::init(APP_ROOT) ) {
+	_exit_html_fail('<h1>Invalid Application Configuration [PDB-029]</h1>', 500);
+}
 
-error_reporting(E_ALL & ~E_NOTICE);
+/**
+ * Singular Database Connection
+ */
+function _dbc()
+{
+	static $dbc;
+
+	if (empty($dbc)) {
+
+		$cfg = \OpenTHC\Config::get('database/main');
+		$dsn = sprintf('pgsql:host=%s;dbname=%s;user=%s;password=%s', $cfg['hostname'], $cfg['database'], $cfg['username'], $cfg['password']);
+
+		// $dbf = sprintf('%s/var/variety.sqlite', APP_ROOT);
+		// $dsn = sprintf('sqlite:%s', $dbf);
+
+		$dbc = new \Edoceo\Radix\DB\SQL($dsn);
+	}
+
+	return $dbc;
+
+}
